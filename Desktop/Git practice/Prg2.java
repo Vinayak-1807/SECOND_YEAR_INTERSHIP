26c996897042fc05bb469e4e20887d2292f74aa3
Vinayak-1807 <nnm24cs330@nmamit.in>
2026-06-01 12:34:04 +0530

    first commit

---
+++ Desktop/Git practice/Prg2.java
@@ -0,0 +1,8 @@
+class Prg2 {
+    public static void main(String[] args) {
+        int a = 1400;
+        int b = 2300;
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
